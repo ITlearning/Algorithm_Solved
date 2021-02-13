a4d7b540e373bc387439d3b935e7a8179b01a468
ITlearning <yo7504@naver.com>
2021-02-13 13:48:53 +0000

    0213

---
+++ BRONZE/02_13/1110.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void newMath(char a) {
+	char *p = &a;
+	int c = 0;
+	int cnt =0;
+	while(p[c] != '\0') {
+		cout << p[c] << endl;
+		c++;
+		if(c == 2) {
+			break;
+		}
+	}
+}
+
+int main() {
+	char a[];
+	for(in)
+	cin >> a;
+	newMath(a);
+}
