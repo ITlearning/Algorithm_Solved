7899f8bb7359995a0487a29fc2eda869a0acc54f
ITlearning <yo7504@naver.com>
2021-02-21 08:58:58 +0000

    0221

--- BRONZE/8000_9000/8958.cpp
+++ BRONZE/8000_9000/8958.cpp
@@ -3,28 +3,20 @@
 using namespace std;
 
 int main() {
-	int tmp = 0;
+	
 	int T;
 	cin >> T;
 	string OX;
-	int cnt[T] = {0};
 	for(int i = 0; i < T; i++) {
 		cin >> OX;
+		int cnt = 0;
+		int sum = 0;
 		for(int j = 0; j < OX.length(); j++) {
-			if(OX[i] == 'O') {
-				int x = i;
-				while(OX[x] == 'O') {
-					tmp = tmp + 1;
-					--x;
-				}
-				cnt[i] += tmp;
-				tmp = 1;
-			}
-			
+			if(OX[j] == 'O') cnt++;
+			else cnt = 0;
+			sum += cnt;
 		}
-	}
-	
-	for(int i = 0; i < T; i++) {
-		cout << cnt[i] << '\n';
+		
+		cout << sum << '\n';
 	}
 }
