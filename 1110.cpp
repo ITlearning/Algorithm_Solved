d6c54a4de2d1cbf8f11549d8f65b0d085ad83472
ITlearning <yo7504@naver.com>
2021-03-09 14:37:05 +0000

    03월 09일 최종 커밋

--- 1110.cpp
+++ 1110.cpp
@@ -3,26 +3,25 @@ using namespace std;
 
 int main() {
 	string n;
+	string b;
 	cin >> n;
+	bool t = false;
 	int a = stoi(n);
 	int tmp = 0;
 	int cnt = 0;
-	/*while(true) {
-		if(a == tmp) {
+	while(true) {
+		if(n == to_string(a) && t == true) {
 			break;
 		} else {
+			if()
+			t = true;
 			int t1 = n[0] - '0';
 			int t2 = n[1] - '0';
-			tmp = t1 + t2;
-			n = n[1] + tmp;
+			tmp = (t1 + t2) % 10;
+			n = to_string(t2) + to_string(tmp);
 			cnt++;
 		}
-	}*/
+	}
 	
-	int t1 = n[0] - '0';
-	int t2 = n[1] - '0';
-	tmp = t1 + t2;
-	
-	cout << tmp << endl;
-	cout << n << endl;
+	cout << cnt << endl;
  }
