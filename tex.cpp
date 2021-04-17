665c30bba7fac295c795b0a42d2cdf4082a59ee2
ITlearning <yo7504@naver.com>
2021-04-17 14:57:08 +0000

    04월 17일 최종 커밋

--- tex.cpp
+++ tex.cpp
@@ -2,86 +2,64 @@
 using namespace std;
 #define X first
 #define Y second
-<<<<<<< HEAD
-int board[502][502];
-bool vis[502][502];
-int n,m;
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1}; // 상하좌우 네 방향을 의미
-
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-	
-  cin >> n >> m;
-  for(int i = 0; i < n; i++)
-    for(int j = 0; j < m; j++)
-      cin >> board[i][j];
-  int mx = 0; // 그림의 최댓값
-  int num = 0; // 그림의 수
-
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){ 
-      if(board[i][j] == 0 or vis[i][j]) continue; 
-      num++; 
-      queue<pair<int,int> > Q;
-      vis[i][j] = 1; 
-      Q.push({i,j});
-      int area = 0;
-      while(!Q.empty()){
-        area++;
-        auto cur = Q.front(); Q.pop();
-        for(int dir = 0; dir < 4; dir++){
-          int nx = cur.X + dx[dir];
-          int ny = cur.Y + dy[dir];
-          if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-          if(vis[nx][ny] || board[nx][ny] != 1) continue;
-          vis[nx][ny] = 1;
-          Q.push({nx,ny});
-        }
-      }
-      mx = max(mx, area);
-=======
-int board[1002][1002];
-int dist[1002][1002];
-int n,m;
+string board[1002];
+int dist1[1002][1002]; // 불의 전파 시간
+int dist2[1002][1002]; // 지훈이의 이동 시간
+int n, m;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  cin >> m >> n;
-  queue<pair<int,int> > Q;
+  cin >> n >> m;
+  for(int i = 0; i < n; i++){
+    fill(dist1[i], dist1[i]+m, -1);
+    fill(dist2[i], dist2[i]+m, -1);    
+  }
+  for(int i = 0; i < n; i++)
+    cin >> board[i];  
+  queue<pair<int,int> > Q1;
+  queue<pair<int,int> > Q2;
   for(int i = 0; i < n; i++){
     for(int j = 0; j < m; j++){
-      cin >> board[i][j];
-      if(board[i][j] == 1)
-        Q.push({i,j});
-      if(board[i][j] == 0)
-        dist[i][j] = -1;
+      if(board[i][j] == 'F'){
+        Q1.push({i,j});
+        dist1[i][j] = 0;        
+      }
+      if(board[i][j] == 'J'){
+        Q2.push({i,j});
+        dist2[i][j] = 0;
+      }
     }
   }
-  while(!Q.empty()){
-    auto cur = Q.front(); Q.pop();
+  // 불에 대한 BFS
+  while(!Q1.empty()){
+    auto cur = Q1.front(); Q1.pop();
     for(int dir = 0; dir < 4; dir++){
       int nx = cur.X + dx[dir];
       int ny = cur.Y + dy[dir];
       if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-      if(dist[nx][ny] >= 0) continue;
-      dist[nx][ny] = dist[cur.X][cur.Y]+1;
-      Q.push({nx,ny});
+      if(dist1[nx][ny] >= 0 || board[nx][ny] == '#') continue; 
+      dist1[nx][ny] = dist1[cur.X][cur.Y]+1;
+      Q1.push({nx,ny});
     }
   }
-  int ans = 0;
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      if(dist[i][j] == -1){
-        cout << -1;
+
+  // 지훈이에 대한 BFS
+  while(!Q2.empty()){
+    auto cur = Q2.front(); Q2.pop();
+    for(int dir = 0; dir < 4; dir++){
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+      if(nx < 0 || nx >= n || ny < 0 || ny >= m){ // 범위를 벗어났다는 것은 탈출에 성공했다는 의미. 큐에 거리 순으로 들어가므로 최초에 탈출한 시간을 출력하면 됨.
+        cout << dist2[cur.X][cur.Y]+1; 
         return 0;
       }
-      ans = max(ans, dist[i][j]);
->>>>>>> fe30384ebe75e7e2e5f8bdef1c1b1ccd24522909
+      if(dist2[nx][ny] >= 0 || board[nx][ny] == '#') continue;
+      if(dist1[nx][ny] != -1 && dist1[nx][ny] <= dist2[cur.X][cur.Y]+1) continue; // 불의 전파 시간을 조건에 추가
+      dist2[nx][ny] = dist2[cur.X][cur.Y]+1;
+      Q2.push({nx,ny});
     }
   }
-  cout << ans;
+  cout << "IMPOSSIBLE"; // 여기에 도달했다는 것은 탈출에 실패했음을 의미.
 }
