ebd99fd0cddcd8d96b5ce7d9feb9cb64b8321ca9
ITlearning <yo7504@naver.com>
2021-04-19 11:28:50 +0000

    04월 19일 중간 커밋

---
+++ 1697_Practice.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dist[100002];
+int n,m;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> m;
+	fill(dist, dist+100001, -1);
+	queue<int> Q;
+	Q.push(n);
+	dist[n] = 0;
+	
+	while(dist[m] == -1) {
+		auto cur = Q.front(); Q.pop();
+		for(int nxt : {cur+1, cur-1, cur*2}){	// range-based for
+			if(nxt < 0 || nxt >= 100000) continue;
+			if(dist[nxt] >= 1) continue;
+			dist[nxt] = dist[cur]+1;
+			Q.push(nxt);
+		}
+	}
+	
+	cout << dist[m] << endl;
+}
+	
--- 1926_Practice.cpp
+++ 1926_Practice.cpp
@@ -13,6 +13,7 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> n >> m;
+
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < m; j++) {
 			cin >> board[i][j];
