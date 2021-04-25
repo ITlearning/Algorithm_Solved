bed9fda9ce01a8fffe7da0edc43ce89f87ebaf26
ITlearning <yo7504@naver.com>
2021-04-25 15:27:47 +0000

    04월 25일 최종 커밋

--- 15652.cpp
+++ 15652.cpp
@@ -4,37 +4,33 @@ using namespace std;
 
 int n,m;
 int board[10];
-int arr[10];
 int isused[10];
 
 void func(int k) {
     if(k == m) {
-		 for(int i = 0; i < m; i++) {
-            cout << arr[i] << ' ';
-        }
-        cout << endl;
-        return; // 리턴을 해서 바로 윗 단계로 올라감
+		for(int i = 0; i < m; i++) {
+			cout << board[i] << ' ';
+		}
+		cout << endl;
+		return;
 	}
 	
 	for(int i = 0; i < n; i++) {
-        if(!isused[i]) { // 아직 사용되지 않은 경우
-            arr[k] = board[i];
-            isused[i] = 1;
-            func(k+1);
-			isused[i] = 0;
-        }
-    }
+		if(!isused[i+1]) {
+			board[k] = i+1;
+			isused[i-1] = 1;
+			isused[i] = 1;
+			func(k+1);
+			for(int j = i ; j < n; j++) {
+				isused[j+1] = 0;
+			}
+		}
+	}
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> m;
-	
-	for(int i = 0; i < n; i++) {
-		cin >> board[i];
-	}
-	sort(board,board+n);
     func(0);
-	
 }
