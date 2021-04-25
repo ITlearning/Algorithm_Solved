e135aa13ce1e131461e784004f6680e6808113a8
ITlearning <yo7504@naver.com>
2021-04-25 15:53:15 +0000

    04월 26일 최종 커밋

---
+++ 15664.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int n,m;
+int board[10];
+int arr[10];
+int isused[10];
+
+void func(int k) {
+    if(k == m) {
+		 for(int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << endl;
+        return; // 리턴을 해서 바로 윗 단계로 올라감
+	}
+	int tmp = 0;
+	for(int i = 0; i < n; i++) {
+        if(!isused[i] && board[i] != tmp) { // 아직 사용되지 않은 경우
+            arr[k] = board[i];
+			tmp = board[i+1];
+            isused[i] = 1;
+            func(k+1);
+			
+			//isused[i] = 0;
+			for(int j = i; j < n; j++) {
+				isused[j+1] = 0;
+			}
+			
+			
+			
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+	
+	for(int i = 0; i < n; i++) {
+		cin >> board[i];
+	}
+	sort(board,board+n);
+    func(0);
+	
+}
