f9f16d203aee2b93f1cc71895ea2a738f1a680d8
ITlearning <yo7504@naver.com>
2021-04-25 14:50:13 +0000

    04월 25일 중간 커밋

--- 15652.cpp
+++ 15652.cpp
@@ -1,25 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
+
 int n,m;
+int board[10];
 int arr[10];
 int isused[10];
 
 void func(int k) {
     if(k == m) {
-        for(int i = 0; i < m; i++) {
+		 for(int i = 0; i < m; i++) {
             cout << arr[i] << ' ';
         }
         cout << endl;
         return; // 리턴을 해서 바로 윗 단계로 올라감
-    }
-
-    for(int i = 1; i <= n; i++) {
+	}
+	
+	for(int i = 0; i < n; i++) {
         if(!isused[i]) { // 아직 사용되지 않은 경우
-            arr[k] = i;
+            arr[k] = board[i];
             isused[i] = 1;
             func(k+1);
-            isused[i] = 0;  // 모두 다 돌고 자기 자신의 수는 사용한게 아니니 false로 만든다.
+			isused[i] = 0;
         }
     }
 }
@@ -27,7 +29,12 @@ void func(int k) {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     cin >> n >> m;
+	
+	for(int i = 0; i < n; i++) {
+		cin >> board[i];
+	}
+	sort(board,board+n);
     func(0);
+	
 }
