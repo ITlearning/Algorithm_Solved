2415524e64a7aa1beb9fcf8b3b7092d43c1fcf1a
ITlearning <yo7504@naver.com>
2021-04-26 08:35:26 +0000

    04월 26일 중간 커밋

--- 1759.cpp
+++ 1759.cpp
@@ -3,22 +3,22 @@ using namespace std;
 #define endl '\n'
 
 int n,m;
-int board[50];
-int arr[50];
+char board[50];
+char arr[50];
 int isused[50];
 
 void func(int k,int index) {
-    if(k == 6) {
-		 for(int i = 0; i < 6; i++) {
-            cout << arr[i] << ' ';
+    if(k == n) {
+		 for(int i = 0; i < n; i++) {
+            cout << arr[i];
         }
         cout << endl;
         return; // 리턴을 해서 바로 윗 단계로 올라감
 	}
 	int used[10000] = {0};
-	for(int i = index; i < n; i++) {
-		if(used[board[i]] == 0){
-			used[board[i]] = 1;
+	for(int i = index; i < m; i++) {
+		if(used[board[i] - 'a'] == 0){
+			used[board[i] - 'a'] = 1;
 			arr[k] = board[i];
 			func(k+1, i+1);
 		}
@@ -28,18 +28,14 @@ void func(int k,int index) {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-	while(true) {
-		cin >> n;
-		if(n == 0) {
-			break;
-		}
-		for(int i = 0; i < n; i++) {
-			cin >> board[i];
-		}
-		sort(board,board+n);
-   		func(0,0);
-		cout << endl;
+	cin >> n >> m;
+	
+	for(int i = 0; i < m; i++) {
+		cin >> board[i];
 	}
+	sort(board,board+m);
+   	func(0,0);
+	cout << endl;
     
 	
 }
