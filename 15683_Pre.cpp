7641d3c2c7c87cf3639e0bda5a054bc454746f43
ITlearning <yo7504@naver.com>
2021-04-30 17:31:10 +0900

    04월 30일 중간 커밋

--- 15683_Pre.cpp
+++ 15683_Pre.cpp
@@ -9,8 +9,11 @@ int cctv_x, cctv_y;
 int lit_x, lit_y;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-int cX[9];
+int dx2[2] = {-1,1};
+int dy2[2] = {};
+int ld[4];
 int cY[9];
+int zero = 0;
 int main()
 {
     ios::sync_with_stdio(0);
@@ -18,151 +21,76 @@ int main()
     queue<pair<pair<int, int>,int>> Q;
     cin >> n >> m;
     int location = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
+    // 이중 for문을 돌며 NxM크기의 직사각형을 만든다.
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
             cin >> board[i][j];
-            if (board[i][j] > 0 && board[i][j] <= 5)
-            {
-                cX[location] = i;
-                cY[location] = j;
-                location++;
-                Q.push({{i, j},board[i][j]});
-            }
-            else if (board[i][j] == 6)
-            {
-                lit_x = i;
-                lit_y = j;
+            if(board[i][j] >= 1 && board[i][j] < 6) { // 이때 1 이상 6이하의 수가 등장했을 경우
+                Q.push({{i,j}, board[i][j]});           // 큐에 위치와 숫자를 push한다.
             }
         }
     }
-    int Gak_move[4] = {};
-    int box_size = 0;
-    int total_small = 0;
+
     while(!Q.empty()) {
-        auto cur = Q.front(); Q.pop();
-        int x = cur.X.X;
-        int y = cur.X.Y;
-        int cN = cur.Y;
-        int cnt = 0;
-        int move = 0;
-        int small = 0;
-        if(cN == 1) {
-            while(cnt < 4) {
-                if(cnt == 0) {
-                    if(y <= m && board[x][y+1] != 6) {
-                        y++;
-                        move++;
-                    } else {
-                        x = cur.X.X;
-                        y = cur.X.Y;
-                        Gak_move[0] = move;
-                        move = 0;
-                        cnt++;
+        auto cur = Q.front();
+        if(cur.Y == 1) {
+            for(int i = 0; i < 4; i++) {
+                int tx = cur.X.X;
+                int ty = cur.X.Y;
+                int cnt = 0;
+                while(tx >= 0 && tx <= n-1 && ty >= 0 && ty <= m-1) {
+                    tx += dx[i];
+                    ty += dy[i];
+                    cnt++;
+                    if(board[tx][ty] == 6) {
+                        break;
                     }
-                } else if(cnt == 1) {
-                    if(x <= n && board[x+1][y] != 6) {
-                        x++;
-                        move++;
-                    } else {
-                        x = cur.X.X;
-                        y = cur.X.Y;
-                        Gak_move[1] = move;
-                        move = 0;
-                        cnt++;
-                    }
-                } else if(cnt == 2) {
-                    if(y > 0 && board[x][y-1] != 6) {
-                        y--;
-                        move++;
-                    } else {
-                        x = cur.X.X;
-                        y = cur.X.Y;
-                        Gak_move[2] = move;
-                        move = 0;
-                        cnt++;
-                    }
-                } else if(cnt == 3) {
-                    if(x > 0 && board[x-1][y] != 6) {
-                        x--;
-                        move++;
-                    } else {
-                        x = cur.X.X;
-                        y = cur.X.Y;
-                        Gak_move[3] = move;
-                        move = 0;
-                        cnt++;
-                    }
+                }
+                cnt -= 1;
+                ld[i] = cnt;
+                cout << "cnt = " << cnt << endl;
+            }
+            int m = 0;
+            for(int i = 1; i < 3; i++) {
+                if(ld[m] < ld[i]) {
+                    m = i;
+                }
+            }
+            cout <<"M:"<< m << endl;
+            int ax = cur.X.X;
+            int ay = cur.X.Y;
+            while(ax >= 0 && ax <= n-1 && ay >= 0 && ay <= m-1) {
+                ax += dx[m];
+                ay += dy[m];
+                cout << "ax : " << ax << endl;
+                cout << "ay: " << ay << endl;
+                cout << endl;
+                if(board[ax][ay] != 6) {
+                    board[ax][ay] = '#';
                 }
             }
 
-            for (int i = 0; i < 4; i++)
-                {
-                    if (Gak_move[i] <= Gak_move[i + 1])
-                    {
-                        small = i;
-                    }
-                    else
-                    {
-                        small = i + 1;
-                    }
-                }
-                
-            if(small == 0) {
-                    for(int i = y; i < m ;i++) {
-                        if(board[x][i] != 6) {
-                            board[x][i] = '#';
-                        } else {
-                            break;
-                        }
-                    }
-                } else if(small == 1) {
-                    for(int i = x; i < n; i++) {
-                        if(board[i][y] != 6) {
-                            board[i][y] = '#';
-                        } else {
-                            break;
-                        }
-                    }
-                } else if(small == 2) {
-                    for(int i = y; i >= 0; i--) {
-                        if(board[x][i] != 6) {
-                            board[x][i] = '#';
-                        } else {
-                            break;
-                        }
-                    }
-                } else if(small == 3) {
-                    for(int i = x; i >= 0; i--) {
-                        if(board[i][y] != 6) {
-                            board[i][y] = '#';
-                        } else {
-                            break;
-                        }
-                    }
-                }
-        } else if(cN == 2) {
+        } else if(cur.Y == 2) {
 
-        } else if(cN == 3) {
+        } else if(cur.Y == 3) {
 
-        } else if(cN == 4) {
+        } else if(cur.Y == 4) {
 
-        } else if(cN == 5) {
+        } else if(cur.Y == 5) {
 
         }
-
+        Q.pop();
     }
-    
-    for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    if (board[i][j] == 0)
-                    {
-                        box_size++;
-                    }
+
+    for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                cout << board[i][j] << ' ';
+                    if(board[i][j] == 0) {
+                        zero++;
                 }
             }
-    cout << box_size << endl;
+            cout << endl;
+        }
+
+    cout << zero << endl;
 }
--- test.cpp
+++ test.cpp
@@ -1,65 +1,12 @@
-#include <iostream>
-#include <queue>
-#include <utility>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
-int board[101][101];
-int check[101][101];
-int rain[101][101];
-
 int main() {
-	ios::sync_with_stdio(NULL);
-	cin.tie(NULL);
-	int N;
-	int heightMax = 0, caseMax = 0;
-	int num = 0;
-	queue<pair<int, int>> Q;
-	cin >> N;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++) {
-			cin >> board[i][j];
-			heightMax = max(heightMax, board[i][j]);
-		}
-
-	for (int i = 0; i <= heightMax; i++) {
-		for (int j = 0; j < N; j++) {
-			for (int k = 0; k < N; k++) {
-				if (board[j][k] <= i) {
-					rain[j][k] = 1; //비에 잠긴 곳이 1 비에 안 잠긴 곳이 0 안 잠긴 곳으로 BFS
-				}
-			}
-		}
-		for (int p = 0; p < N; p++) {
-			for (int q = 0; q < N; q++) {
-				if (rain[p][q] == 0 && check[p][q] == 0) {
-					num++;
-					check[p][q] = 1;
-					Q.push({ p,q });
-					while (!Q.empty()) {
-						auto cur = Q.front();
-						Q.pop();
-						for (int idx = 0; idx < 4; idx++) {
-							int nx = cur.first + dx[idx];
-							int ny = cur.second + dy[idx];
-							if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-							if (check[nx][ny] == 1 || rain[nx][ny] == 1) continue;
-							check[nx][ny] = 1;
-							Q.push({ nx,ny });
-						}
-					}
-				}
-			}
-		}
-		caseMax = max(caseMax, num);
-		num = 0;
-		for (int p = 0; p < N; p++)
-			for (int q = 0; q < N; q++) {
-				rain[p][q] = 0;
-				check[p][q] = 0;
-			}
+	int ax = 5;
+	int ac[2] = {-1, 1}; 
+	int cnt = 0;
+	while(ax > 0 && ax <= 10) {
+		ax += ac[1];
+		cout << cnt++ << endl;
 	}
-	cout << caseMax;
 }
