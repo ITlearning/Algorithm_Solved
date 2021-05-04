778c07af950e3810252ab4076751f5faafab764c
ITlearning <yo7504@naver.com>
2021-05-04 17:50:07 +0900

    05월 04일 중간 커밋

---
+++ 11931.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MX 2000001
+#define endl '\n'
+int freq[MX];
+int n;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int m;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        freq[a + 1000000]++;
+    }
+    int a = 1;
+    for(int i = 2000000; i >= 0; i--) {
+        while(freq[i]--) {
+            cout << i-1000000 << '\n';
+        }
+    }
+}
