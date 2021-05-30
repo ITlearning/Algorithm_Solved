3eacd3a434b84cbd4a5c6d0a7babb242be71d0b1
ITlearning <yo7504@naver.com>
2021-05-30 14:31:11 +0900

    05월 30일 알고리즘

---
+++ Python/ThisIsCodingTest/PreviousProblems/Implementation/09.py
@@ -0,0 +1,29 @@
+# 문자열 압축
+
+def solution(s):
+    length = []
+    result = ""
+    max_line = 0
+    
+    if len(s) == 1:
+        return 1
+    
+    for i in range(1,len(s)//2 + 1) :
+        cnt = 1
+        tempStr = s[:i]
+        for j in range(i, len(s), i):
+            if s[j:j+i] == tempStr:
+                cnt += 1
+            else:
+                if cnt == 1:
+                    cnt = ""
+                result += str(cnt) + tempStr
+                tempStr = s[j:j+i]
+                cnt = 1
+        if cnt == 1:
+            cnt = ""
+        result += str(cnt) + tempStr
+        length.append(len(result))
+        result = ""
+        
+    return min(length)
