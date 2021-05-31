73d507f3fc5b7f8b38d7d77d573a2f3386b94fc0
ITlearning <yo7504@naver.com>
2021-05-31 16:44:48 +0900

    이진 탐색 문제 2

---
+++ Python/ThisIsCodingTest/PreviousProblems/BinarySearch/P-28.py
@@ -0,0 +1,22 @@
+# 고정점 찾기
+
+def binary_search(array,n,start,end) :
+    while start <= end:
+        mid = (start+end) // 2
+        if(mid == array[mid]) :
+            return mid
+        elif array[mid] > n :
+            end = mid - 1
+        else :
+            start = mid + 1
+    return None
+
+n = int(input())
+board = list(map(int,input().split()))
+
+result = binary_search(board,n,0,n)
+
+if result == None :
+    print("-1")
+else:
+    print(result)
