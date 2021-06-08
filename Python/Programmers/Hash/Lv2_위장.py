ecab86c7af250bf93e6144d4fdf7f17e3f8a3664
ITlearning <yo7504@naver.com>
2021-06-08 17:45:21 +0900

    프로그래머스 문제 풀이법 푸시

--- Python/Programmers/Hash/Lv2_위장.py
+++ Python/Programmers/Hash/Lv2_위장.py
@@ -1,7 +1,6 @@
 from itertools import combinations
 from collections import Counter
 def solution(clothes):
-    answer = 0
     b = []
     for i in clothes:
         b.append(i[1])
@@ -13,8 +12,21 @@ def solution(clothes):
     
     if len(valuelist) == 1:
         return valuelist[0]
+    
     tmp = valuelist[0]
     for i in valuelist[1:]:
         tmp += i + (i*tmp)
         
     return tmp
+
+# 공식을 알아야 한다. 공식이 항이 늘어날때마다 이전 겨로가에 더해지는 형식
+'''
+예) [1]
+1 = 1
+
+예) [1 2]
+1 + (2 + 2 * 1) = 1 + 4 = 5
+
+예) [1 2 3]
+1 + (2 + 2 * 1) + (3 + 3 * 5) = 1 + 4 + 18 = 23
+'''
