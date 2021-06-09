45b593d8768079c1a98a4753880798c43262a977
ITlearning <yo7504@naver.com>
2021-06-10 00:58:13 +0900

    프로그래머스 힙 문제 풀이

---
+++ Python/Programmers/Heap/Lv3_이중우선순위큐.py
@@ -0,0 +1,22 @@
+# https://codedrive.tistory.com/54
+import heapq
+
+def solution(operations):
+    answer = []
+    for i in operations:
+        a,b = i.split()
+        if a == 'I':
+            heapq.heappush(answer, int(b))
+        else:
+            if len(answer) > 0:
+                if b == '1':
+                    answer.pop(answer.index(heapq.nlargest(1,answer)[0])) 
+                    # nlargest : n개의 가장 큰 값들로 이루어진 리스트
+                    # 첫번째 인자에 찾고싶은 최대값의 개수를 넣어주면 된다.
+                else:
+                    heapq.heappop(answer)
+                    
+    if len(answer) == 0:
+        return [0,0]
+    else:
+        return [heapq.nlargest(1,answer)[0], answer[0]]
