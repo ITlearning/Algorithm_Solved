8756f66fc1c3e729e24239889c016f4ee09d39c4
ITlearning <yo7504@naver.com>
2021-06-10 22:28:59 +0900

    백준 10825 문제

---
+++ Python/BOJ/10825.py
@@ -0,0 +1,20 @@
+# 국영수
+# 처음에는 무식하게 그냥 if 문으로 나눠서 했더니 시간초과가 나왔다.
+# 파이썬에는 람다로 다 처리가 가능하다;
+import sys
+input = sys.stdin.readline
+t = int(input())
+board = []
+
+def change(i):
+    board[i], board[i+1] = board[i+1], board[i]
+    return 1
+
+for _ in range(t):
+    name, korean, english, math = input().split()
+    board.append([name, korean, english, math])
+
+board.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for i in board:
+    print(i[0])
