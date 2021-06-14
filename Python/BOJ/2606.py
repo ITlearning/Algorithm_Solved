38a27d86cd1997af4d967b2532bccab7373ae172
ITlearning <yo7504@naver.com>
2021-06-14 17:22:36 +0900

    BOJ 2606 바이러스 문제

---
+++ Python/BOJ/2606.py
@@ -0,0 +1,26 @@
+# 바이러스
+import sys
+input = sys.stdin.readline
+n = int(input())
+v = int(input())
+board = []
+graph = {}
+
+for _ in range(v) :
+    a,b = map(int, input().split())
+    board.append([a,b])
+
+for i,j in board:
+    graph[i] = graph.get(i, []) + [j]
+    graph[j] = graph.get(j, []) + [i]
+
+# 재귀적으로 인덱스를 지난다.
+def dfs(start, graph):
+    # for문을 돌며 지나간 인덱스가 아닐 경우 추가 해주고, 재귀적으로 해당 인덱스를 돈다.
+    for i in graph[start]:
+        if i not in dist:
+            dist.append(i)
+            dfs(i, graph)
+dist = []
+dfs(1, graph)
+print(len(dist)-1)
