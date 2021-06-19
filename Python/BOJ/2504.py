a056ec45d63dd121dbb8047115c238f2a6ad6fed
ITlearning <yo7504@naver.com>
2021-06-19 12:20:57 +0900

    BOJ 2504 괄호의 값

--- Python/BOJ/2504.py
+++ Python/BOJ/2504.py
@@ -3,35 +3,38 @@ import sys
 input = sys.stdin.readline
 board = list(input().rstrip())
 stack = []
+circle = []
+box = []
 tmp = 1
 result = 0
-impossible = False
-print(board)
+t = False
+
 for i in range(len(board)):
-    if board[i] == '(':
-        stack.append(board[i])
-        tmp *= 2
-    elif board[i] == '[':
+    if board[i] == '[':
         stack.append(board[i])
         tmp *= 3
-    elif board[i] == ')' and (stack[-1] != '(' or len(stack) == 0):
-        impossible = True
-        break
-    elif board[i] == ']' and (stack[-1] != '[' or len(stack) == 0):
-        impossible = True
-        break
-    elif board[i] == ']':
-        if board[i-1] == '[':
-            result += tmp
-            
-        tmp //= 3
+    elif board[i] == '(':
+        stack.append(board[i])
+        tmp *= 2
     elif board[i] == ')':
+        if len(stack) == 0 or stack[-1] == '[' :
+            t = True
+            break
+
         if board[i-1] == '(':
             result += tmp
+        stack.pop()
         tmp //= 2
-
-
-if impossible or len(stack) != 0:
+    elif board[i] == ']':
+        if len(stack) == 0 or stack[-1] == '(' :
+            t = True
+            break 
+        if board[i-1] == '[':
+            result += tmp
+        stack.pop()
+        tmp //= 3
+if t or len(stack):
     print(0)
 else:
     print(result)
+    
