6f000115397fe87f556a0112437c232a2c7349ef
ITlearning <yo7504@naver.com>
2021-06-23 21:40:08 +0900

    프로그래머스 Lv2 문자열만들기 문제:

---
+++ Python/Programmers/Lv2_Problem/Lv2_JadenCase문자열만들기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = ''
+    tmp = ""
+    for i in range(len(s)):
+        if s[i].isalpha() and s[i-1] == " " or i == 0:
+            answer += s[i].upper()
+        elif s[i].isalpha() and (s[i-1].isalpha() or not s[i-1].isalpha()):
+            answer += s[i].lower()
+        else:
+            answer += s[i]
+        
+    
+    return answer
