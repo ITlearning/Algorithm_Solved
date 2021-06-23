d76e73059d335b98506cd2dcf76520daa7adcb96
ITlearning <yo7504@naver.com>
2021-06-23 21:42:46 +0900

    프로그래머스 문제 풀이

--- Python/Programmers/Lv2_Problem/Lv2_JadenCase문자열만들기.py
+++ Python/Programmers/Lv2_Problem/Lv2_JadenCase문자열만들기.py
@@ -1,6 +1,5 @@
 def solution(s):
     answer = ''
-    tmp = ""
     for i in range(len(s)):
         if s[i].isalpha() and s[i-1] == " " or i == 0:
             answer += s[i].upper()
---
+++ Python/Programmers/Lv2_Problem/Lv2_피보나치수.py
@@ -0,0 +1,10 @@
+def solution(n):
+    
+    dp = [0] * 100001
+    
+    dp[1] = 1
+    dp[2] = 1
+    for i in range(3,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+        
+    return dp[n] % 1234567
