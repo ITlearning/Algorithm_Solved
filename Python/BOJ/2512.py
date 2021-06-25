d5b218d8ea43decc853bf1e48fa0ad6c8b0e8d87
ITlearning <yo7504@naver.com>
2021-06-25 21:45:01 +0900

    BOJ 2512 예산 문제

---
+++ Python/BOJ/2512.py
@@ -0,0 +1,31 @@
+# 예산
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+board = list(map(int,input().split()))
+topic = int(input())
+start = 0
+end = topic
+answer = 0
+if sum(board) <= topic:
+    answer = max(board)
+else:
+    while start <= end:
+        mid = (start+end) // 2
+        result = 0
+        print(mid)
+        for i in board:
+            if i > mid:
+                result += mid
+            else:
+                result += i
+        print("result :",result)
+        if result <= topic:
+            answer = mid
+            start = mid + 1
+        elif result > topic:
+            end = mid - 1
+
+print(answer)
