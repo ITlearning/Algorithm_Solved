8db9910d1ca9a39ec32f6047846aa8668770920b
ITlearning <yo7504@naver.com>
2021-06-26 16:38:00 +0900

    BOJ 1697 숨바꼭질 문제 파이썬 풀이

--- Python/BOJ/1697.py
+++ Python/BOJ/1697.py
@@ -2,23 +2,18 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-
+dist = [-1] * 100001
 n,k = map(int,input().split())
 
-def bfs(n,k):
-    q = deque()
-    q.append((n))
-
-    board = [0] * 100001
-
-    while q:
-        x = q.popleft()
-        if x == k:
-            return board[x]
-        for i in (x-1,x+1, x*2):
-            if 0 <= i < 100001 and board[i] == 0:
-                board[i] = board[x]+1
-                q.append(i)
-
-
-print(bfs(n,k))
+q = deque()
+q.append(n)
+while q:
+    x = q.popleft()
+    if x == k:
+        print(dist[x]+1)
+        break
+    for nx in (x-1, x+1, x*2):
+        if 0 <= nx <= 100000:
+            if dist[nx] == -1:
+                dist[nx] = dist[x] + 1
+                q.append(nx)
