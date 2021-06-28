2a499de0f9c26d44d08f239a7371ea0dad2a2514
ITlearning <yo7504@naver.com>
2021-06-28 16:57:14 +0900

    BOJ 5014 스타트링크 문제

--- Python/BOJ/5014.py
+++ Python/BOJ/5014.py
@@ -1,25 +1,29 @@
 # 스타트링크
 import sys
 from collections import deque
-sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
+# F 빌딩의 총 층
+# S 강호가 현재 위치하는 층
+# G 스타트링크가 있는 층
+# U 위로 U층 이동할 수 있는 수
+# D 아래로 D층 이동할 수 있는 수
 F,S,G,U,D = map(int,input().split())
 
-cnt = 0
-def dfs(F,S,G,U,D):
-    q = deque()
-    q.append([S,0])
+q = deque()
+q.append([S,0])
+
+def dfs():
     dist = {S}
     while q:
-        floor ,cnt = q.popleft()
-        if floor == G:
+        x, cnt = q.popleft()
+        if x == G:
             return cnt
-        if floor + U <= F and floor + U not in dist:
-            q.append([floor + U, cnt + 1])
-            dist.add(floor + U)
-        if floor - D >= 1 and floor - D not in dist:
-            q.append([floor - D, cnt + 1])
-            dist.add(floor - D)
+        if x + U <= F and x + U not in dist:
+            q.append([x+U, cnt+1])
+            dist.add(x+U)
+        if x - D >= 1 and x - D not in dist:
+            q.append([x-D, cnt + 1])
+            dist.add(x-D)
     return "use the stairs"
 
-print(dfs(F,S,G,U,D))
+print(dfs())
