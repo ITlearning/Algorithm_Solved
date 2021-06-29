624dec14f72b3634572994693435766d5606cad1
ITlearning <yo7504@naver.com>
2021-06-29 13:03:53 +0900

    프로그래머스 Lv3 보석쇼핑 문제 풀이

---
+++ Python/Programmers/Lv3_Problem/Lv3_보석쇼핑.py
@@ -0,0 +1,33 @@
+def solution(gems):
+    answer = []
+    gem_len = len(gems) + 1
+    overlap = len(set(gems))
+    start = 0
+    end = 0
+    t = 0
+    dic = {}
+    
+    while end < len(gems):
+        
+        if gems[end] not in dic:
+            dic[gems[end]] = 1  # 추가
+        else:
+            dic[gems[end]] += 1 # 이미 있으면 수 증가
+            
+        end += 1
+        
+        if len(dic) == overlap:
+            while start < end:
+                if dic[gems[start]] > 1:
+                    dic[gems[start]] -= 1
+                    start += 1
+                
+                elif gem_len > end - start:
+                    gem_len = end - start
+                    answer = [start+1, end]
+                    break
+                
+                else:
+                    break
+    
+    return answer
