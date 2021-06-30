179f731032c0bb1f10406aca2819868a6fa38c62
ITlearning <yo7504@naver.com>
2021-06-30 10:58:03 +0900

    BOJ Lv3 불량 사용자 문제

---
+++ Python/Programmers/Lv3_Problem/Lv3_불량사용자.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+
+def match(per, banned_id):
+    for i in range(len(per)):
+        if len(per[i]) != len(banned_id[i]):
+            return False
+        for j in range(len(per[i])):
+            if banned_id[i][j] == '*':
+                continue
+            if per[i][j] != banned_id[i][j]:
+                return False
+    return True
+
+def solution(user_id, banned_id):
+    answer = 0
+    ans = []
+    for i in permutations(user_id, len(banned_id)):
+        if match(i, banned_id):
+            i = set(i)
+            if i not in ans:
+                ans.append(i)
+    
+    return len(ans)
