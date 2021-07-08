664a3206e24ce0322a3a5ff2988e04c6b395da62
IT Learning <yo7504@kakao.com>
2021-07-08 15:35:48 +0900

    BOJ 4889 안정적인 문자열 파이썬 풀이

--- Python/BOJ/1755.py
+++ Python/BOJ/1755.py
@@ -1,24 +1,31 @@
 # 숫자놀이
 import sys
 input = sys.stdin.readline
+# 각 숫자들의 문자들 리스트
 alphabet = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+# 숫자와 문자 한번에 저장하는 리스트
 board = []
+
 n,m = map(int,input().split())
 
+# board 리스트에 숫자와 그 숫자에 해당하는 문자열 같이 저장하는 for 문
 for i in range(n, m+1):
-    tmp = ""
-    if i >= 10:
-        t = str(i)
-        for j in t:
-            tmp += alphabet[int(j)]
+    tmp = ""        # 숫자가 10 이상일 경우 저장할 문자열
+    if i >= 10:     # 10 이상일 경우
+        t = str(i)  # 문자열로 변환
+        for j in t: # 하나씩 뜯어서 변환
+            tmp += alphabet[int(j)] # tmp 에 저장
             tmp += " "
-        board.append([i, tmp])
-    else:
-        board.append([i, alphabet[i]])
+        board.append([i, tmp]) # 변환이 끝나면 board에 숫자와 같이 저장
+    else: # 10보다 작을 경우
+        board.append([i, alphabet[i]]) # 그냥 저장
 
-board.sort(key=lambda x: x[1])
+board.sort(key=lambda x: x[1]) # 문자열을 기준으로 사전순 정렬
+
+# 출력
 for i in range(len(board)):
-    if i % 10 == 0 and not i == 0:
+    if i % 10 == 0 and not i == 0: # 10씩 끊어서 줄 넘기고 출력
         print()
         print(board[i][0], end=' ')
     else:
---
+++ Python/BOJ/4889.py
@@ -0,0 +1,33 @@
+# 안정적인 문자열
+import sys
+input = sys.stdin.readline
+cnt = 1 # 순서 카운트 변수
+
+
+while True:
+    s = [] # 스택역할 리스트
+    text = input().rstrip()
+    change = 0 # 바꾼 수
+
+    for i in text:       # 입력받은 괄호 하나씩 돌면서
+        if i == "{":     # 만일 괄호가 { 일 경우
+            s.append(i)  # 일단 스택에 추가
+        elif i == "}":   # 반대로 } 일 경우
+            if len(s) == 0 or s[-1] != "{": # 스택이 비어있거나, 마지막으로 넣은게 반대괄호가 아닐 경우
+                s.append(i) # 추가
+            else: # 그게 아니고 짝이 맞는 경우
+                s.pop() # 짝 맞춰진거니 빼줌
+        else: # 괄호 이외의 것이 들어오면 프로그램 종료
+            exit(0)
+
+    while s:        # 안정적이지 않은 괄호들의 스택
+        tmp = s[-1] # 제일 마지막에 넣은 것
+        s.pop()     # 하나 빼주고
+        if s[-1] != tmp: # 마지막에 넣은게 그 전 스택이랑 다를 경우 ex) }{
+            change += 2  # 2 추가
+        else: # 그게 아니고 같을 경우 ex) {{ , }}
+            change += 1  # 1 추가
+        s.pop() # 비교가 끝났으니 마저 pop 해줌
+    
+    print(cnt,". ",change, sep='') # 결과 출력
+    cnt += 1
