64d397351f71977bfd4d33faa2c039ca92048a16
IT Learning <yo7504@kakao.com>
2021-07-17 14:28:17 +0900

    BOJ 2331 문제 풀이

--- Python/BOJ/2331.py
+++ Python/BOJ/2331.py
@@ -4,20 +4,23 @@ input = sys.stdin.readline
 
 a,p = map(int, input().split())
 
-next = a+1
-
-D = []
-tmp = a
-while True:
-    t = str(tmp)
+d = 0
+D = [a]
+tmp = str(a)
+while D.count(int(tmp)) < 2:
     total = 0
-    for i in t:
-        total += pow(int(i), p)
-    if total not in D:
-        D.append(total)
-    else:
-        tmp = total
-        break
-    tmp = total
-
-print(tmp)
+    if len(tmp) == 1:
+        total += pow(int(tmp), p)
+    elif len(tmp) == 2:
+        total = pow(int(tmp[0]), p) + pow(int(tmp[1]), p)
+    elif len(tmp) == 3:
+        total = pow(int(tmp[0]), p) + pow(int(tmp[1]), p) + pow(int(tmp[2]), p)
+    elif len(tmp) == 4:
+        total = pow(int(tmp[0]), p) + pow(int(tmp[1]), p) + pow(int(tmp[2]), p) + pow(int(tmp[3]), p)
+    elif len(tmp) == 5:
+        total = pow(int(tmp[0]), p) + pow(int(tmp[1]), p) + pow(int(tmp[2]), p) + pow(int(tmp[3]), p) + pow(int(tmp[4]), p)
+    elif len(tmp) == 6:
+        total = pow(int(tmp[0]), p) + pow(int(tmp[1]), p) + pow(int(tmp[2]), p) + pow(int(tmp[3]), p) + pow(int(tmp[4]), p) + pow(int(tmp[5]), p)
+    tmp = str(total)
+    D.append(total)
+print(D.index(int(tmp)))
