01e84123eea8130c116ea4910e0ed625dca1c0b9
IT Learning <yo7504@kakao.com>
2021-07-18 21:12:01 +0900

    BOJ 15658 문제

--- Python/BOJ/15658.py
+++ Python/BOJ/15658.py
@@ -1,1 +1,30 @@
 # 연산자 끼워넣기
+import sys
+input = sys.stdin.readline
+
+mini = 999999999999
+maxi = -999999999999
+
+n = int(input())
+num = list(map(int,input().split()))
+plus, minus, mul, div = map(int,input().split())
+
+def dfs(index, result, plus, minus, mul, div):
+    global maxi, mini
+    if index == n:
+        maxi = max(maxi, result)
+        mini = min(mini, result)
+        return
+    if plus > 0:
+        dfs(index+1, result + num[index], plus-1, minus, mul, div)
+    if minus > 0:
+        dfs(index+1, result - num[index], plus, minus-1, mul, div)
+    if mul > 0:
+        dfs(index+1, result * num[index], plus, minus, mul-1, div)
+    if div > 0:
+        dfs(index+1, int(result / num[index]), plus, minus, mul, div-1)
+
+dfs(1, num[0], plus, minus, mul, div)
+
+print(maxi)
+print(mini)
