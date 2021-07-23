05781da4d936dbe03dae7e8f28200fc09d66caeb
IT Learning <yo7504@kakao.com>
2021-07-23 22:34:45 +0900

    BOJ 1120 문자열 문제

--- Python/BOJ/1120.py
+++ Python/BOJ/1120.py
@@ -1,10 +1,30 @@
 # 문자열
+
+# 계속 틀린거
+"""
 import sys
 input = sys.stdin.readline
+# sys 를 사용하면 뒤에 \n 이 들어가서 아마도 문제가 되는듯 하다.
+# 이걸 계속 알면서도 삽질했다 하;
 
 a,b = input().split()
 
-total = 999999999999
+total = 1e9
+cur = len(b)-len(a)
+for i in range(cur+1):
+    cnt = 0
+    for j in range(len(a)):
+        if a[j] != b[j+i]:
+            cnt += 1
+    total = min(total, cnt)
+
+print(total)
+"""
+
+# 정답
+a,b = input().split()
+
+total = 1e9
 cur = len(b)-len(a)
 for i in range(cur+1):
     cnt = 0
