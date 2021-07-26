8adea6f186a8ed889348406d9c26ef7015a1f31c
IT Learning <yo7504@kakao.com>
2021-07-26 12:30:35 +0900

    BOj 15686 치킨 배달 문제 풀이

--- Python/BOJ/15686.py
+++ Python/BOJ/15686.py
@@ -1,10 +1,11 @@
 # 치킨 배달
-from itertools import permutations
+from itertools import combinations
 N,M = map(int,input().split())
 
 board = []
 people = []
 shop = []
+
 for i in range(N):
     board.append(list(map(int,input().split())))
 
@@ -15,31 +16,21 @@ for i in range(N):
         elif board[i][j] == 2:
             shop.append([i+1,j+1])
 
-brute = []
-for b in range(len(shop)):
-    if b < M:
-        brute.append(0)
-    else:
-        brute.append(1)
-
-print(brute)
-
 k = 0
-small = 1e9
+small = []
 
-n = list(permutations(brute))
+brute = list(combinations(shop,M))
 
-for permution in n:
+for permution in brute:
     total = 0
     for p in people:
-        tmp = 1e9
-        for i in range(len(shop)):
-            if permution[i] == 0:
-                continue
-            chicken = abs(shop[i][0] - p[0]) + abs(shop[i][1] - p[1])
-            tmp = min(tmp, chicken)
-            total += tmp
-        small = min(small,total)
+        tmp = []
+        for i in permution:
+            tmp.append(abs(i[0]-p[0]) + abs(i[1]-p[1]))
+        tmp.sort()
+        total += tmp[0]
+    small.append(total)
 
-print(small)
+small.sort()
+print(small[0])
 
