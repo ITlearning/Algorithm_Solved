87fe56fcd58ae81b81e2769d39d55cae5b962c9c
IT Learning <yo7504@kakao.com>
2021-07-31 13:01:55 +0900

    BOJ 2437 저울 문제

--- Python/BOJ/2437.py
+++ Python/BOJ/2437.py
@@ -6,8 +6,26 @@ N = int(input())
 
 board = list(map(int,input().split()))
 board.sort()
-total = []
-for i in range(0,N-1):
-    if board[i] not in total:
-        total.append(board[i])
-    for j in range()
+total = 1
+for i in board:
+    if total < i:
+        break
+    total += i
+
+print(total)
+'''
+for i in range(0,N): # index
+    for cur in range(1,N): #cursor
+        for j in range(0,len(board), cur):
+            if board[i] != board[j]:
+                tmp = board[i] + sum(board[j:j+cur])
+                if tmp not in total:
+                    total.append(tmp)
+total.sort()
+print(total)
+for i in total:
+    if i+1 not in total:
+        print(i+1)
+        break
+'''
+
