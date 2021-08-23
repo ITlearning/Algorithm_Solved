557cd4d657953eff6a84efd7f92a56f534d977a8
IT Learning <yo7504@kakao.com>
2021-08-23 13:01:16 +0900

    BOJ 16112 5차 전직 문제

--- Python/BOJ/16112.py
+++ Python/BOJ/16112.py
@@ -4,20 +4,24 @@ import heapq
 input = sys.stdin.readline
 
 n,k = map(int,input().split())
-t = list(map(int,input().split()))
-board = []
-for i in t:
-    heapq.heappush(board,-i)
+answer = []
+board = list(map(int,input().split()))
 
-total = []
 
-num = 0
+for i in board:
+    heapq.heappush(answer, i)
 
+total = 0 # 전체 값
+cur = 0 # 인덱스 숫자 카운트
+
+# 돌면서 인덱스 수 만큼 곱해준다.
+# 어짜피 로직상, 작은 것부터 뽑아서 그 인덱스 수에 맞게 곱해주면 답이 나온다.
+
+# ex) 100 * 0 , 200 * 1, 300*2 = 800
 for i in range(n):
-    tmp = heapq.heappop(board)
-    if len(board) > k-1:
-        num += num
-        num += abs(tmp)
-        #total.append(tmp)
+    tmp = heapq.heappop(answer)
+    total += tmp*cur
+    if cur < k:
+        cur += 1
 
-print(num)
+print(total)
