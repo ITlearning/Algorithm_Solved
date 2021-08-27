b184d6152e302751d1ee8d614d47df1f32f195f1
IT Learning <yo7504@kakao.com>
2021-08-27 15:53:46 +0900

    BOJ 1300 K번째 수

--- Python/BOJ/1300.py
+++ Python/BOJ/1300.py
@@ -5,5 +5,22 @@ input = sys.stdin.readline
 n = int(input())
 k = int(input())
 
-print(7/3)
-print(int(round(k/3, 1)*n))
+start, end = 1, k
+answer = 0
+while start <= end:
+    mid = (start+end) // 2
+    tmp = 0
+    # A[i][j] 에서 i의 행에 속한 숫자들은 i*j이므로 모두 i의 배수.
+    # 각 행과 mid를 나눌경우, 그 행에 mid 보다 작은 수의 개수가 나옴.
+    # 그 개수와 n의 수를 비교해 더 작은 수 기입.
+    # 그렇게 하면 mid 수보다 작은 숫자들의 개수가 나옴.
+    for i in range(1,n+1):
+        tmp += min(mid//i, n)
+
+    if tmp >= k:
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(answer)
