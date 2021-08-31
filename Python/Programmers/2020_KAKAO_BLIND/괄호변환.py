2456b889831e9af4341e53b8852ad4081b86439a
IT Learning <yo7504@kakao.com>
2021-08-31 20:00:01 +0900

    중간 저장

--- Python/Programmers/2020_KAKAO_BLIND/괄호변환.py
+++ Python/Programmers/2020_KAKAO_BLIND/괄호변환.py
@@ -1,19 +1,44 @@
+# 3. 올바른 괄호 문자열인지 체크
+def right(s):
+    cnt = 0
+    for i in s:
+        if i == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+        
+        if cnt < 0:
+            return False
+    return True
 
+# 4. 괄호 바꾸기
+def reverse(s):
+    tmp = ''
+    for i in s:
+        if i == '(':
+            tmp += ')'
+        else:
+            tmp += '('
+            
+    return tmp
+    
 
 def solution(p):
-    answer = ""
-    total = []
-    # 1. 입력이 빈 문자열의 경우, 빈 문자열을 반환합니다
+    # 1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다.
     if len(p) == 0:
         return ""
     
+    cnt = 0
     for i in range(len(p)):
-        total.append(p[i])
-        
-
-    return total
-
-p = "()))((()"
-t = solution(p)
-
-print(t)
+        if p[i] == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt == 0:
+            u = p[:i+1]
+            v = p[i+1:]
+            break
+    if right(u):
+        return u + solution(v)
+    else:
+        return '(' + solution(v) + ')' + reverse(u[1:-1])
--- Python/Programmers/2020_KAKAO_BLIND/순위검색.py
+++ Python/Programmers/2020_KAKAO_BLIND/순위검색.py
@@ -1,292 +1,84 @@
-from itertools import permutations
+from itertools import permutations, product
 from itertools import combinations
+
+def binary_search(point, item):
+    start = 1
+    end = len(item)
+    #print(point)
+    #print(item)
+    answer = 0
+    while start <= end:
+        mid = (start + end) // 2
+        #print(item[mid-1][1], point)
+        if item[mid-1][1] >= point:
+            answer = mid
+            start = mid + 1
+        else:
+            end = mid - 1
+    return answer
+
+
+def cc(copy, i, items, index, tu):
+    for k in range(0,4):
+        i[k] = '-'
+        repl = tuple(i[:4])
+        for j in product(repl, repeat=4):
+            if j in tu:
+                if len(items[j]) != 0:
+                    if [index,int(i[4])] not in items[j] :
+                        items[j].append([index,int(i[4])])       
+                else:
+                    if [index,int(i[4])] not in items[j]:
+                        items[j].append([index,int(i[4])])          
+            i = copy[:]
+
 def solution(info, query):
     answer = []
     player = []
+    items = {}
     que = []
-    items = {
-        'cpp': 
-        {
-            'backend': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            },
-            'frontend': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            },
-
-            '-': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                '-':
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            }
-        },
-        
-        'java':
-        {
-            'backend': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            },
-            'frontend': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            },
-
-            '-': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                '-':
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            }
-        },
-        
-        'python':
-        {
-            'backend': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            },
-            'frontend': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            },
-
-            '-': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                '-':
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            }
-        },
-
-        '-': 
-        {
-            'backend': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            },
-            'frontend': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            },
-
-            '-': 
-            {
-                'junior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                'senior': 
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                },
-                '-':
-                {
-                    'pizza': [],
-                    'chicken': [],
-                    '-': []
-                }
-            }
-        }
-
-    }
+    language = ['cpp','java', 'python', '-']
+    location = ['frontend','backend', '-']
+    year = ['junior','senior', '-']
+    food = ['chicken', 'pizza', '-']
+    tu = []
     
+    for i in product(language, location, year, food):
+        #print(i)
+        tu.append(i)
+        items[i] = []
+        
     
     for i in info:
         tmp = i.split()
         player.append(tmp)
     
-    for p in player:
-        language = ['cpp','java', 'python', '-']
-        location = ['frontend','backend', '-']
-        year = ['junior','seinor', '-']
-        food = ['chicken', 'pizza', '-']
-
-        items[p[0]][p[1]][p[2]][p[3]].append(p[4])
-        p[0] = '-'
-        tmp = list(permutations(p[:4],4))
-        #print(tmp)
-        for i in tmp:
-            #print(i)
-            if (i[0] in language) and (i[1] in location) and (i[2] in year) and (i[3] in food) :
-                items[i[0]][i[1]][i[2]][i[3]].append(p[4])   
+    for index,i in enumerate(player):
+        d = tuple(i[:4])
+        copy = i[:]
+        items[d].append([index,int(i[4])])
+        cc(copy,i,items, index, tu)
         
-        p[1] = '-'
-        tmp = list(permutations(p[:4],4))
-        #print(tmp)
-        for i in tmp:
-            #print(i)
-            if (i[0] in language) and (i[1] in location) and (i[2] in year) and (i[3] in food) :
-                items[i[0]][i[1]][i[2]][i[3]].append(p[4])       
-        p[2] = '-'
-        tmp = list(permutations(p[:4],4))
-        #print(tmp)
-        for i in tmp:
-            #print(i)
-            if (i[0] in language) and (i[1] in location) and (i[2] in year) and (i[3] in food) :
-                items[i[0]][i[1]][i[2]][i[3]].append(p[4]) 
-        p[3] = '-'
-        tmp = list(permutations(p[:4],4))
-        #print(tmp)
-        for i in tmp:
-            #print(i)
-            if (i[0] in language) and (i[1] in location) and (i[2] in year) and (i[3] in food) :
-                items[i[0]][i[1]][i[2]][i[3]].append(p[4]) 
-
-    #print(items)
     
-    for q in query:
-        qq = q.replace("and", "")
+    for i in query:
+        qq = i.replace("and", "")
         tmp = qq.split()
         que.append(tmp)
-    cnt = 1
-    for i in que:
-        print(i[0],i[1],i[2],i[3])
-        print(items[i[0]][i[1]][i[2]][i[3]])
-        print()
+        #print(tmp)
+        #que.append(t)
         
-            
+    for q in que:
+        check = tuple(q[:4])
+        items[check].sort(reverse=True,key=lambda x: int(x[1]))
+        #print(items[check])
+        p = binary_search(int(q[4]), items[check])
+        answer.append(p)
+        
+    
     
     return answer
 
-solution(["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"], ["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"])
+
+info = ["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"]
+query = ["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"]
+print(solution(info,query))
