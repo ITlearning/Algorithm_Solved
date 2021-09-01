2730736b16cc931e0acccb5c2e54892872e2070a
IT Learning <yo7504@kakao.com>
2021-09-01 16:17:07 +0900

    [2020 카카오 블라인드] 자물쇠와 열쇠 문제

--- Python/Programmers/2020_KAKAO_BLIND/자물쇠와열쇠.py
+++ Python/Programmers/2020_KAKAO_BLIND/자물쇠와열쇠.py
@@ -1,5 +1,6 @@
-
 def turn(key):
+    return list(zip(*key[::-1]))
+    '''
     n = len(key)
     m = len(key[0])
     tmp = [[0]*n for _ in range(m)]
@@ -7,26 +8,55 @@ def turn(key):
         for j in range(m):
             tmp[j][n-i-1] = key[i][j]
     return tmp
+    '''
+
+
+def insert(x,y,turn_key, double_lock, M):
+    for i in range(M):
+        for j in range(M):
+            #print(double_lock)
+            double_lock[x+i][y+j] += turn_key[i][j]
+
+def reload(x,y,turn_key, double_lock, M):
+    for i in range(M):
+        for j in range(M):
+            #print(double_lock)
+            double_lock[x+i][y+j] -= turn_key[i][j]
+
+def check(N,M, double_lock):
+    for i in range(N):
+        for j in range(N):
+            if double_lock[M+i][M+j] != 1:
+                return False
+    return True
 
 
 def solution(key, lock):
-    answer = True
-    k = 0
-    while k < 4:
-        turn_key = turn(key)
-        print(turn_key)
+    n = len(lock)
+    m = len(key)
+    double_lock = [[0] * (m*2+n) for _ in range(m*2+n)] 
 
-        #for i in range(len(turn_key[0])):
-            
+    for i in range(n):
+        for j in range(n):
+            double_lock[m+i][m+j] = lock[i][j]
 
-        key = turn_key[:]
-        k+= 1
+    
+    turn_key = key
+    for _ in range(4):
+        turn_key = turn(turn_key)
+        for i in range(1, n+m):
+            for j in range(1, n+m):
+                insert(i,j,turn_key,double_lock, m)
+                if (check(n,m,double_lock)):
+                    return True
+                reload(i,j,turn_key,double_lock, m)
 
-        
+    return False
 
-    return answer
 
 key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
 lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
 
-solution(key, lock)
+print(solution(key, lock))
+
+# 2시간 35분 56초
