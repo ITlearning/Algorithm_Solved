309c8c1888b4c4ea2091382d7387210aa60c36e5
ITlearning <yo7504@naver.com>
2021-09-13 17:57:50 +0900

    후보키 문제 풀이중

--- Python/Programmers/2019_KAKAO_BLIND/후보키.py
+++ Python/Programmers/2019_KAKAO_BLIND/후보키.py
@@ -1,21 +1,39 @@
 from itertools import combinations
 def solution(relation):
-    #turn = list(zip(*relation[::1]))
-    #for i in turn:
-        #print(i)
-    total = set()
-    cnt = 0
-    t = []
-    line = [1,2,3,4]
-    if len(relation) != 0:
-        col = len(relation[0])
-        row = len(relation)
+    answer = []
+    board = []
+    dic = {}
 
-    for i in range(1,col+1):
-            # extend
-            t.extend([set(k) for k in combinations([j for j in range(col)],i)])
-    print(t)
-    #print(cnt) 
-    return 0
+    # 유일성
+    for i in range(len(relation)):
+        t = []
+        for j in range(1,len(relation[0])+1):
+            tmp = list(combinations(relation[i], j))
+            #print(tmp)
+            tmp.sort()
+            if tmp not in board:
+                board.append(tmp)
+    
+    #print(board)
+    for i in range(len(board)-1,-1,-1):
+        print(set(board[i]))
+        print(set(board[i-1]))
+        cnt = True
+        for j in range(len(board[i])-1,-1,-1):
+            for k in range(len(board[i-1])):
+                print(set(board[i-1][j]), set(board[i][k]))
+                print(set(board[i-1][j]).issubset(set(board[i][k])))
+                
+                if set(board[i-1][j]).issubset(set(board[i][k])):
+                    cnt = False
+                    break
+        
+        if cnt : 
+            if len(board[i]) not in answer:
+                answer.append(len(board[i][0]))
+
+            
+
+    return answer
 
 print(solution([["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"], ["400", "con", "computer", "4"], ["500", "muzi", "music", "3"], ["600", "apeach", "music", "2"]]))
