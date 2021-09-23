44cb79a83d2435ccc3a8bf3780b5a231e18fe913
ITlearning <yo7504@naver.com>
2021-09-23 13:20:09 +0900

    BOJ 2110 문제 풀이

--- Python/BOJ/2110.py
+++ Python/BOJ/2110.py
@@ -1,1 +1,41 @@
 # 공유기 설치
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+n,c = map(int,input().split())
+
+print(n,c)
+
+board = []
+
+for _ in range(n):
+    num = int(input())
+    board.append(num)
+board.sort()
+
+def binary_search(start,end):
+    while start <= end:
+
+        mid = (start + end) // 2
+        select = board[0]
+        count = 1
+        for i in range(1,len(board)):
+            if board[i] >= mid + select:
+                count += 1
+                select = board[i]
+
+        if count >= c:
+            global answer
+            answer = mid
+            start = mid + 1
+        else:
+            end = mid - 1
+
+
+answer = 0
+start = 1
+end = board[-1] - board[0]
+binary_search(start,end)
+
+print(answer)
