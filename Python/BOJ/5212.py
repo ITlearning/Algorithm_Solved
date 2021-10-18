e25312f531c7af638c13ab86e0134ec1c117998a
ITlearning <yo7504@naver.com>
2021-10-18 17:03:36 +0900

    [BOJ] 5212 지구 온난화 문제 풀이

--- Python/BOJ/5212.py
+++ Python/BOJ/5212.py
@@ -5,8 +5,48 @@ board = []
 
 dx = [0,-1,0,1]
 dy = [-1,0,1,0]
-
+# 1. 입력
 for i in range(r):
     board.append(list(str(input())))
 
-print(board)
+# 2. 주위 바다 채우기
+
+delete = []
+
+for i in range(r):
+    for j in range(c):
+        if board[i][j] == "X":
+            cnt = 0
+            for dir in range(4):
+                nx = i + dx[dir]
+                ny = j + dy[dir]
+                if nx < 0 or ny < 0 or nx >= r or ny >= c:
+                    cnt += 1
+                    continue
+                if board[nx][ny] != ".":
+                    continue
+                cnt += 1
+            if cnt >= 3:
+                delete.append([i,j])
+    
+for x,y in delete:
+    board[x][y] = "."
+
+start_x = 99999999999
+end_x = 0
+start_y = 99999999999
+end_y = 0
+
+for i in range(r):
+    for j in range(c):
+        if board[i][j] == "X":
+            start_x = min(start_x, i)
+            start_y = min(start_y, j)
+            end_x = max(end_x,i)
+            end_y = max(end_y,j)
+
+
+for i in range(start_x, end_x+1):
+    for j in range(start_y, end_y+1):
+        print(board[i][j], end="")
+    print()
