23b6c46a82f1805c477044acc44f7dbf7b10beb0
ITlearning <yo7504@naver.com>
2021-11-08 22:37:10 +0900

    [BOJ] 13335 트럭 문제

--- Python/BOJ/13335.py
+++ Python/BOJ/13335.py
@@ -1,2 +1,26 @@
 # 트럭
+from collections import deque
+N,W,L = map(int,input().split())
 
+board = list(map(int,input().split()))
+
+answer = 0
+bridge = [0]*(W)
+truck_weights = deque(board)
+bridge = deque(bridge)
+cnt = 0
+total = 0
+
+while len(bridge) != 0:
+    num = bridge.popleft()
+    total -= num
+    cnt += 1
+    
+    if truck_weights:
+        if total + truck_weights[0] <= L:
+            m = truck_weights.popleft()
+            total += m
+            bridge.append(m)
+        else:
+            bridge.append(0)
+print(cnt)
--- Python/Test.py
+++ Python/Test.py
@@ -1,72 +1,24 @@
-def reload(num,b,sx,sy,ex,ey, point):
-    if num == 1:
-        tmp = sy
-        while sx < ex+1:
-            if sy > ey:
-                sx += 1
-                sy = tmp
-                continue
-            b[sx][sy] -= point
-            sy += 1
+from collections import deque
 
-        '''
-        for i in range(sx, ex+1):
-            for j in range(sy,ey+1):
-                b[i][j] -= point
-        '''
-    elif num == 2:
-        tmp = sy
-        while sx < ex+1:
-            if sy > ey:
-                sx += 1
-                sy = tmp
-                continue
-            b[sx][sy] += point
-            sy += 1
-        '''
-        for i in range(sx, ex+1):
-            for j in range(sy,ey+1):
-                b[i][j] += point
-        '''
-
-def binary_search(answer, target, start, end):
-    total = 0
-    while start < end:
-        mid = (start+end) // 2
-        if answer[mid] < target:
-            total = mid
-            start = mid + 1
-        elif answer[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return total
-
-def solution(board, skill):
+def solution(bridge_length, weight, truck_weights):
     answer = 0
-
-    for i in skill:
-        # 적 입력 시
-        if i[0] == 1:
-            start_x, start_y = i[1],i[2]
-            end_x, end_y = i[3],i[4]
-            attack = i[5]
-            reload(i[0],board,start_x,start_y,end_x,end_y, attack)
-        # 아군 입력 시
-        elif i[0] == 2:
-            start_x, start_y = i[1],i[2]
-            end_x, end_y = i[3],i[4]
-            heal = i[5]
-            reload(i[0],board,start_x,start_y,end_x,end_y, heal)
-
-
-    an = sum(board,[])
-    an.sort()
-    #print(an)
-    total = len(an)
-    total -= binary_search(an,1,0,len(an)-1) + 1
-
-    return total
-
-print(solution(	[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[1, 1, 1, 2, 2, 4], [1, 0, 0, 1, 1, 2], [2, 2, 0, 2, 0, 100]]))
-print(solution([[5, 5, 5, 5, 5], [5, 5, 5, 5, 5], [5, 5, 5, 5, 5], [5, 5, 5, 5, 5]], [[1, 0, 0, 3, 4, 4], [1, 2, 0, 2, 3, 2], [2, 1, 0, 3, 1, 2], [1, 0, 1, 3, 3, 1]]))
+    bridge = [0]*(bridge_length)
+    truck_weights = deque(truck_weights)
+    bridge = deque(bridge)
+    cnt = 0
+    total = 0
+    while len(bridge) != 0:
+        num = bridge.popleft()
+        total -= num
+        
+        cnt += 1
+        
+        if truck_weights:
+            if total + truck_weights[0] <= weight:
+                m = truck_weights.popleft()
+                total += m
+                bridge.append(m)
+            else:
+                bridge.append(0)
+                   
+    return cnt
