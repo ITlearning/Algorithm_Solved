92a42d2be2c07fc24569538fcfd155f3e91f175b
ITlearning <yo7504@naver.com>
2021-11-16 01:46:29 +0900

    [BOJ] 1743 음식물 피하기 문제

---
+++ Python/BOJ/1743_Study.py
@@ -0,0 +1,38 @@
+# 음식물 피하기
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,m,k = map(int,input().split())
+
+board = [[0 for _ in range(m)] for _ in range(n)]
+
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+for _ in range(k):
+    a,b = map(int,input().split())
+    board[a-1][b-1] = 1
+answer = 0
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 0:
+            continue
+        q = deque()
+        q.append([i,j])
+        board[i][j] = 2
+        cnt = 1
+        while q:
+            x,y = q.popleft()
+            for cur in range(4):
+                nx = x + dx[cur]
+                ny = y + dy[cur]
+                if 0 <= nx < n and 0 <= ny < m:
+                    if board[nx][ny] == 0 or board[nx][ny] == 2:
+                        continue
+                    board[nx][ny] = 2
+                    q.append([nx,ny])
+                    cnt += 1
+        #print(cnt)
+        answer = max(answer, cnt)
+print(answer)
