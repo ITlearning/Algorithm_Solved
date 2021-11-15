76bfb00e084e51105eb7e040fb4a4df4687d9281
ITlearning <yo7504@naver.com>
2021-11-15 16:39:12 +0900

    [BOJ] 2002, 19538 문제 풀이

---
+++ Python/BOJ/19583.py
@@ -0,0 +1,34 @@
+# 개강총회
+
+S,E,Q = input().split()
+
+S = S.split(":")
+S = int("".join(S))
+
+E = E.split(":")
+E = int("".join(E))
+
+Q = Q.split(":")
+Q = int("".join(Q))
+
+board = {}
+check = []
+cnt = 0
+while True:
+    try:
+        time, nick_name = input().split()
+        time = time.split(":")
+        time = int("".join(time))
+        #print(time, E)
+        if time <= S or nick_name not in board:
+            board[nick_name] = time
+        else:
+            #print(board[nick_name], S)
+            if board[nick_name] <= S:
+                if E <= time <= Q and nick_name not in check:
+                    check.append(nick_name)
+                    cnt += 1
+    except:
+        break
+
+print(cnt)
--- Python/BOJ/2002.py
+++ Python/BOJ/2002.py
@@ -1,15 +1,21 @@
 # 추월
-
 n = int(input())
 board = {}
-pick = []
-for i in range(n):
-    board[input()] = i+1
+for i in range(1,n+1):
+    board[input()] = i
 
+answer = []
+
+min_num = min(board.values())
+cnt = 0
 for _ in range(n):
     tmp = input()
-    pick.append(board[tmp])
-    del board[tmp]
-    print(board)
+    if min_num != board[tmp]:
+        cnt += 1
+        del board[tmp]
+    else:
+        del board[tmp]
+        if board:
+            min_num = min(board.values())
 
-print(pick)
+print(cnt)
