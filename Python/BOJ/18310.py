3daf8d1301cb8b902efd97e0f2b00f9bd65b9190
ITlearning <yo7504@naver.com>
2021-12-06 16:56:07 +0900

    [BOJ] 18310 안테나 문제 풀이

--- Python/BOJ/18310.py
+++ Python/BOJ/18310.py
@@ -5,20 +5,6 @@ n = int(input())
 board = list(map(int,input().split()))
 max_len = max(board)
 
-start = 0
-end = max_len
-answer = 999999999999
+board.sort()
 
-while start <= end:
-    mid = (start + end) // 2
-    tmp = 0
-    for i in board:
-        tmp += abs(i - mid)
-
-    if tmp < answer:
-        answer = tmp
-        start += mid + 1
-    else:
-        end = mid - 1
-
-print(answer)
+print(board[(n-1) // 2])
