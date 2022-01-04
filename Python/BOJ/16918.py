f73faf1ccc1279611a1e0dff96732c245ee13cdb
ITlearning <yo7504@naver.com>
2022-01-04 21:34:44 +0900

    [BOJ] 문제 주석 처리

--- Python/BOJ/16918.py
+++ Python/BOJ/16918.py
@@ -8,6 +8,7 @@ board = []
 dx = [0,-1,0,1]
 dy = [-1,0,1,0]
 
+# 입력받는것과 동시에 O 위치 덱에 저장
 for i in range(R):
     tmp = list(input().rstrip())
     board.append(tmp)
@@ -15,18 +16,26 @@ for i in range(R):
         if tmp[j] == "O":
             q.append([i,j])
 
+# 전체가 다 O로 되어있는 리스트
 all_circle = [["O" for _ in range(C)] for _ in range(R)]
 cnt = 0
+
+# cnt가 N이 될때까지 돌리기
 while cnt < N:
+    # 제일 처음 카운트가 시작했으면
     if cnt == 0:
+        # 그냥 카운트 + 1 해주고 continue
         cnt += 1
         continue
-
+    
+    # 지금 초 가 홀수면
     if cnt % 2 == 1:
+        # 모든 위치 O로 변환해주고 카운트 + 1
         board = deepcopy(all_circle)
         cnt += 1
         continue
 
+    # O 위치 저장했던 덱 가져와서 O위치랑 상하좌우 .으로 바꾸기
     while q:
         x,y = q.popleft()
         board[x][y] = "."
@@ -37,14 +46,16 @@ while cnt < N:
                 if board[nx][ny] == 'O':
                     board[nx][ny] = '.'
     
+    # 그리고 다시 O 위치 체크
     for i in range(R):
         for j in range(C):
             if board[i][j] == 'O':
                 q.append([i,j])
 
+    # 카운트 + 1
     cnt += 1
     
-
+# 답 출력
 for bo in board:
     print("".join(bo))
     
--- Python/BOJ/9935.py
+++ Python/BOJ/9935.py
@@ -1,16 +1,22 @@
 # 문자열 폭발
 from copy import deepcopy
 text = input()
-tmp = deepcopy(text)
 match_text = input().rstrip()
 stack = []
+
 for i in text:
+    # stack에 글자 하나 추가
     stack.append(i)
+    # 추가한 글자와 찾아야할 단어 마지막 글자가 같고, 현재 모아진 문자 중 찾아야할 글자의 길이만큼 대조해서 찾아야할 글자라면
     if i == match_text[-1] and "".join(stack[-len(match_text):]) == match_text:
         for _ in range(len(match_text)):
+            # 찾아야할 글자만큼 뽑아내기
             stack.pop()
 
+# 그렇게 해서 stack이 남아있다면
 if stack:
+    # 출력
     print("".join(stack))
 else:
+    # 남아있지 않으면 출력
     print("FRULA")
